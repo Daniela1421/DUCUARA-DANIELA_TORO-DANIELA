ecef9a40eb867812851afcfd643c516812a202d5
daniela1421 <daniela.ducuara1817@gmail.com>
2024-08-08 23:16:52 -0500

    Add OdontologoDAOImplMemory in main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import DAO.Implements.DB;
+import DAO.Implements.IDAO;
 import Modelo.Odontologo;
 import Servicio.OdontologoServicio;
 
@@ -9,5 +10,19 @@ public class Main {
         OdontologoDAOH2 odontologoIDAO = new OdontologoDAOH2();
         OdontologoServicio odontologoServicio = new OdontologoServicio(odontologoIDAO);
         odontologoServicio.guardar(odontologo);
+
+        IDAO<Odontologo> odontologoDao = new OdontologoDAOImplMemory();
+
+        Odontologo odontologo1 = new Odontologo(1, "001", "Juan", "Pérez");
+        Odontologo odontologo2 = new Odontologo(2, "002", "Ana", "Gómez");
+
+        odontologoDao.guardar(odontologo1);
+        odontologoDao.guardar(odontologo2);
+
+        Odontologo encontrado = odontologoDao.buscarPorId(1);
+        System.out.println("Odontólogo encontrado: " + encontrado.getNombre() + " " + encontrado.getApellido());
+
+        List<Odontologo> todos = ((OdontologoDAOImplMemory)odontologoDao).listarTodos();
+        todos.forEach(o -> System.out.println(o.getNombre() + " " + o.getApellido()));
     }
 }
