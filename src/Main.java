3aa9765e854fbf22f930fc1f3756d8c36c33f790
daniitoro29 <daniitoro29@gmail.com>
2024-08-08 23:30:29 -0500

    names changed

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ import Servicio.OdontologoServicio;
 public class Main {
     public static void main(String[] args) {
         DB.createTable();
-        Odontologo odontologo = new Odontologo(1, "123", "Cristian", "Dominguez");
-        Odontologo odontologo1 = new Odontologo(2, "345","David", "Herrera");
+        Odontologo odontologoH2 = new Odontologo(1, "123", "Cristian", "Dominguez");
+        Odontologo odontologo1H2 = new Odontologo(2, "345","David", "Herrera");
         OdontologoDAOH2 odontologoIDAO = new OdontologoDAOH2();
         OdontologoServicio odontologoServicio = new OdontologoServicio(odontologoIDAO);
-        odontologoServicio.guardar(odontologo);
-        odontologoServicio.guardar(odontologo1);
+        odontologoServicio.guardar(odontologoH2);
+        odontologoServicio.guardar(odontologo1H2);
         odontologoServicio.listarTodos();
     }
 }
